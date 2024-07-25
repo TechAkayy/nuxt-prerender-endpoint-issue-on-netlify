1ab8d1551cbb1b4c1f094a90e9c4c4bf2258a803
Akayy <techakayy@gmail.com>
2024-07-25 23:52:09 +1000

    With default layout & index page

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <v-app>
-    <!-- WORKS -->
-    <HotDeals class="mt-8" />
-    <!-- DOESN'T WORK -->
-    <v-main class="py-4">
-      <HotDeals class="mt-8" />
-    </v-main>
-  </v-app>
+  <NuxtLayout>
+    <NuxtPage> </NuxtPage>
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <!-- WORKS -->
+    <HotDeals class="mt-8" />
+    <!-- DOESN'T WORK -->
+    <v-main class="py-4">
+      <HotDeals class="mt-8" />
+    </v-main>
+  </div>
+</template>
+
+<style scoped></style>
