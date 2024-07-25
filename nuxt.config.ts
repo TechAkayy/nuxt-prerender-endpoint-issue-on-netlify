62362179d40ba6c681c58a4667ffcc87327eb2a8
Akayy <techakayy@gmail.com>
2024-07-25 22:26:26 +1000

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,42 @@
+import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: {enabled: true},
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    renderJsonPayloads: true,
+    payloadExtraction: true,
+    sharedPrerenderData: true,
+  },
+
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins.push(vuetify({autoImport: true}))
+      })
+    },
+    //...
+  ],
+  vite: {
+    server: {
+      cors: true, // Only for dev
+    },
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+    build: {
+      minify: false,
+    },
+  },
+  sourcemap: true,
+})
