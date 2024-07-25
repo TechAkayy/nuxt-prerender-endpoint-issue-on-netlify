66aacb49053bf94813cedea4bbfee9d86925abed
Akayy <techakayy@gmail.com>
2024-07-26 01:18:08 +1000

    Nav fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,29 @@
+<script setup lang="ts">
+  const route = useRoute()
+  const links = reactive([
+    {
+      name: 'Home',
+      url: '/',
+      active: computed(() => route.fullPath === '/'),
+    },
+    {
+      name: 'Products',
+      url: '/products',
+      active: computed(() => route.fullPath === '/products'),
+    },
+  ])
+</script>
+
 <template>
   <div class="p-4">
     <nav class="space-x-4">
-      <NuxtLink class="underline" to="/">Home</NuxtLink>
-      <NuxtLink class="underline" to="products">Products</NuxtLink>
+      <NuxtLink
+        v-for="link in links"
+        :class="{underline: link.active}"
+        :to="link.url"
+        active=""
+        >{{ link.name }}</NuxtLink
+      >
     </nav>
     <main class="py-4">
       <slot />
