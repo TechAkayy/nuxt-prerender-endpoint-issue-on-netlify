fb3ba7838c729418c63986990c6688a5e4f22452
Akayy <techakayy@gmail.com>
2024-09-15 00:29:21 +1000

    No prerenderRoutes

--- pages/products.vue
+++ pages/products.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-  // const {data: jobs, error} = await useFetch('https://api.nuxt.com/jobs')
+  const {data: jobs} = await useFetch('https://api.nuxt.com/jobs')
 
-  const {data, error} = await useFetch('/api/store')
+  const {data} = await useFetch('/api/store')
 
-  prerenderRoutes(`/api/store`)
+  // prerenderRoutes(`/api/store`)
 
   const {products} = data.value
 </script>
@@ -11,6 +11,7 @@
 <template>
   <div>
     <p>Total products: {{ products.length }}</p>
+    <p>Total jobs: {{ jobs.length }}</p>
   </div>
 </template>
 
